0fa30eca11002364ab1cea7b82b50e3d5c1f1818
MdAdnan21 <mohammedadnan.3107@gmail.com>
2023-07-19 17:45:53 +0530

    changes

--- src/component/Testimonial.js
+++ src/component/Testimonial.js
@@ -3,32 +3,35 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <>
-    <div id='moreabout'>
-      <h3 id='Get'>GET YOUR QUESTION ANSWER</h3>
-      <h1>Frequently asked question</h1>
-      <div className='details'>   <details>
-        <summary>How to contact with riders emergency  </summary>
-        <p id='line1'>Get your website ads tests delivered at let collect sample from the victory.</p>
+      <div id='moreabout'>
+        <h3 id='Get'>GET YOUR QUESTION ANSWER</h3>
+        <h1>Frequently asked question</h1>
+        <div className='combination'>
+          <div className='details'>
+            <details>
+              <summary>How to contact with riders emergency <i class="fa-solid fa-plus"></i></summary>
+              <p id='line1'>Get your website ads tests delivered at let collect sample from the victory.</p>
+            </details>
 
-      </details>
+            <details>
+              <summary>App installation failed, how to update system information? <i class="fa-solid fa-plus"></i></summary>
+              <p id='line2'>Rhino skin maybe thick but it can be quite sensitive to sunburns and insect bites</p>
 
-        <details>
-          <summary>App installation failed, how to update system information?</summary>
-          <p id='line2'>Rhino skin maybe thick but it can be quite sensitive to sunburns and insect bites</p>
+            </details>
 
-        </details>
-        <details>
-          <summary>Website response taking time, how to improve? </summary>
-          <p id='line3'>Get your blood tests delivered at the home collect a sample from management news..</p>
+            <details>
+              <summary>Website response taking time, how to improve?  <i class="fa-solid fa-plus"></i> </summary>
+              <p id='line3'>Get your blood tests delivered at the home collect a sample from management news..</p>
 
-        </details>
-        <details>
-          <summary>New update fixed all bug and issues</summary>
-          <p id='line4'>If you are looking to hunt a unicorn, but dont know where to  Lake Superior.</p>
+            </details>
 
-        </details>
+            <details>
+              <summary>New update fixed all bug and issues  <i class="fa-solid fa-plus"></i></summary>
+              <p id='line4'>If you are looking to hunt a unicorn, but dont know where to  Lake Superior.</p>
 
-      </div>
+            </details>
+          </div>
+        </div>
       </div>
     </>
   )
